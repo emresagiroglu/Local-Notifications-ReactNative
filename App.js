d09d1e49e20b3f28df4b2fadd0c10c74aafe0f01
Emre Sağıroğlu <94112475+emresagiroglu@users.noreply.github.com>
2024-01-05 14:46:38 +0300

    Local Notifications Added

--- App.js
+++ App.js
@@ -1,10 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+import * as Device from "expo-device";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    };
+  },
+});
+
+const allowsNotificationsAsync = async () => {
+  const settings = await Notifications.getPermissionsAsync();
+  return (
+    settings.granted ||
+    settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+  );
+};
+
+const requestPermissionsAsync = async () => {
+  return await Notifications.requestPermissionsAsync({
+    ios: {
+      allowAlert: true,
+      allowBadge: true,
+      allowSound: true,
+      allowAnnouncements: true,
+    },
+  });
+};
 
 export default function App() {
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED");
+        console.log(notification);
+      }
+    );
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response);
+      }
+    );
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
+  const notificationHandler = async () => {
+    if (Device.osName === "iOS") {
+      const hasPushNotificationPermissionGranted =
+        await allowsNotificationsAsync();
+
+      if (!hasPushNotificationPermissionGranted) {
+        await requestPermissionsAsync();
+      }
+    }
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "First Local Notification",
+        body: "This is body section",
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Send Notification" onPress={notificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +82,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
